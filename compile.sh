b3af553e257e47fca9ae68f3013f0fca94a3eb65
Jaliborc <contact@jaliborc.com>
2014-05-29 00:57:36 +0100

    no message

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 lex ijcompiler.l
 yacc -d -v ijcompiler.y
-gcc -o ijparser y.tab.c lex.yy.c -ll -ly
+gcc -o ijcompiler y.tab.c lex.yy.c -ll -ly
 #./ijparser < demo7.java
 #./ijparser -t < demo.java
 #./ijparser -t < demo6.java
